859f8b35adc5cb1822d3efa2cf63cbd80ab8b95d
Piero Sifuentes <piero9212@gmail.com>
2018-02-08 00:14:39 -0500

    Resolving conflicts

---
+++ Example/BankCardKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BankCardKit
+//
+//  Created by piero9212@gmail.com on 02/08/2018.
+//  Copyright (c) 2018 piero9212@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
